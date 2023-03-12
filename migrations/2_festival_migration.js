e29edb3f5c33585dbc5cd2f858329d9f898df6d0
Dev King <126171640+devking877@users.noreply.github.com>
2023-03-12 09:35:56 -0400

    Update 2_festival_migration.js

--- migrations/2_festival_migration.js
+++ migrations/2_festival_migration.js
@@ -1,6 +1,6 @@
 const FestToken = artifacts.require("FestToken");
 const FestiveTicketsFactory = artifacts.require("FestiveTicketsFactory");
-
+//import
 module.exports = function (deployer) {
   deployer.deploy(FestToken);
   deployer.deploy(FestiveTicketsFactory);
